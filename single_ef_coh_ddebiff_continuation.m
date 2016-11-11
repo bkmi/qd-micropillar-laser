a46bbd1fa2cefeda7f71c0cd660a34c7ebc6932d
Ben M <benmiller1315@gmail.com>
2016-11-11 14:16:40 +0100

    Consolidate into one ddebif continuation
    add option for which parameter to continue.
    
    consolidate both phase and amplitude continuation into one file.

--- single_ef_coh_ddebiff_continuation.m
+++ single_ef_coh_ddebiff_continuation.m
@@ -1,54 +1,60 @@
 %This program runs for the single mode laser case.
 
 % Setup steady state and start with continuation.
+% continue_choice variable determines which paramter is continued.
 
-%{
-%FOR FEED PHASE
-%plot names
-plot_param_name = 'Feedback Phase';
-plot_param_unit = '(no units)';
+if continue_choice == 1
+  %DEFINE FEED PHASE
+  %plot names
+  plot_param_name = 'Feedback Phase';
+  plot_param_unit = '(no units)';
 
-%prepare ddebif
-par_cont_ind = [ind_feed_phase,ind_omega];
-[branch1,suc]=SetupStst(funcs,'contpar',par_cont_ind,'corpar',ind_omega,...
-    'x',xx_guess,'parameter',par,opt_inputs{:},...
-    'max_step',[ind_feed_phase,2*pi/16] ,'max_bound',[ind_feed_phase,8*pi],'newton_max_iterations',10);
-branch1.method.continuation.plot=0;
-[branch1,s,f,r]=br_contn(funcs,branch1,500);
-%}
+  %prepare ddebif
+  par_cont_ind = [ind_feed_phase,ind_omega];
+  [branch1,suc]=SetupStst(funcs,'contpar',par_cont_ind,'corpar',ind_omega,...
+      'x',xx_guess,'parameter',par,opt_inputs{:},...
+      'max_step',[ind_feed_phase,2*pi/16] ,'max_bound',[ind_feed_phase,8*pi],'newton_max_iterations',10);
+      
+elseif continue_choice == 2
+  %DEFINE FEED AMPLI
+  %plot names
+  plot_param_name = 'Feedback Ampli';
+  plot_param_unit = '(no units)';
 
+  %prepare ddebif
+  par_cont_ind = [ind_feed_ampli,ind_omega];
+  [branch1,suc]=SetupStst(funcs,'contpar',par_cont_ind,'corpar',ind_omega,...
+      'x',xx_guess,'parameter',par,opt_inputs{:},...
+      'max_step',[ind_feed_ampli,0.01] ,'max_bound',[ind_feed_ampli,0.99],'newton_max_iterations',10);
+      
+else
+error('make a choice about continue_choice in options')
 
-%FOR FEED AMPLI
-%plot names
-plot_param_name = 'Feedback Ampli';
-plot_param_unit = '(no units)';
+end
 
-%prepare ddebif
-par_cont_ind = [ind_feed_ampli,ind_omega];
-[branch1,suc]=SetupStst(funcs,'contpar',par_cont_ind,'corpar',ind_omega,...
-    'x',xx_guess,'parameter',par,opt_inputs{:},...
-    'max_step',[ind_feed_ampli,0.01] ,'max_bound',[ind_feed_ampli,0.99],'newton_max_iterations',10);
-branch1.method.continuation.plot=0;
+%create continuation and plot
+branch1.method.continuation.plot=1;
+figure(2); clf;
 [branch1,s,f,r]=br_contn(funcs,branch1,100);
-
+title(strcat('Omega-vs-', plot_param_name))
+xlabel(strcat(plot_param_name,{' '}, plot_param_unit))
+ylabel('Omega (?? units)')
 
 
 % --
 
 
 % Access values at data points from continuation.
-
 branch1_plot_vals = c_extract(branch1, par_cont_ind);
 
 
 % Create plots versus modified params
-figure(2); clf;
+figure(3); clf;
 subplot(2,2,[1,2]);
 plot(branch1_plot_vals.par(1,:), arrayfun(@(x)norm(x),branch1_plot_vals.soln(1,:)+1i*branch1_plot_vals.soln(2,:)))
 title({strcat('Electric Field Amplitude-vs-', plot_param_name); strcat('with J=',num2str(J,'%1.1e'),'A')})
 xlabel(strcat(plot_param_name,{' '}, plot_param_unit))
 ylabel(strcat({'|E(t)| '}, ef_units))
-
 
 subplot(2,2,3);
 plot(branch1_plot_vals.par(1,:),branch1_plot_vals.soln(3,:))
@@ -74,8 +80,8 @@ branch1.method.stability.minimal_real_part = -3.0
 'exclude_trivial',true,'locate_trivial',@(p)0,'funcs',funcs);
 
 %from lina's example, plot real eigenvalue part versus parameter
-figure(3); clf;
-[x_measure,y_measure]= df_measr(1,branch1)
+figure(4); clf;
+[x_measure,y_measure]= df_measr(1,branch1);
 br_plot(branch1,x_measure,y_measure);
 title(strcat('Real part of Eigenvalues-vs-', plot_param_name))
 xlabel(strcat(plot_param_name,{' '}, plot_param_unit))
--- single_ef_coh_options.m
+++ single_ef_coh_options.m
@@ -8,6 +8,9 @@ addpath('~/dde_biftool_v3.1.1/ddebiftool_extra_rotsym');
 %dim_choice = 1; % dimensional units determined in setup_params
 dim_choice = 2; % dimensionless, @Oct 31 2016 the dimensionless unit of time was counting nanoseconds with tau_sp
 
+%SELECT DDEBIF CONTINUATION
+continue_choice = 1; % 'Feedback phase: feed_phase'
+%continue_choice = 2; % 'Feedback amplitude: feed_ampli'
 
 %HIST VECTOR
 hist=[1e-9;0;0;0];
@@ -15,8 +18,8 @@ hist=[1e-9;0;0;0];
 
 %TIME SECTION (Depends on unit choice!!!)
 %feedback/delay params
-feed_phase = pi; %phase from ott10
-feed_ampli = 0.00;
+feed_phase = 0;
+feed_ampli = 0.55;
 tau_fb 	   = 0.8;	%units set with dim choice
 %time bound
 min_time = 0;	%units set with dim choice
@@ -26,4 +29,4 @@ max_time = 15;	%units set with dim choice
 % dde23 solver options
 % https://www.mathworks.com/help/matlab/ref/ddeset.html#f81-1031913
 %,'RelTol',0.01,'InitialStep', 0.001e-12, 'MaxStep', 0.01e-12, 'OutputFcn',@odeplot,
-options = ddeset('RelTol',10^-8) , 'OutputFcn', @odeplot
+options = ddeset('RelTol',10^-8) %, 'OutputFcn', @odeplot
