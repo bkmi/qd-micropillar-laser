6f702c17532bdca3aef5074a4de35c6c273d5b36
Ben M <benmiller1315@gmail.com>
2016-11-29 16:31:11 +0100

    Add param scanner in other direction, add in-progress eval_branch which will zoom in on a branch section

---
+++ functions/alternate_param_scanner.m
@@ -0,0 +1,22 @@
+function [ scanned_branch, nunst_scanned_branch ] = alternate_param_scanner( branch_point, funcs, scan_point_count, scan_parm_ind, step_bound_options, min_real_part )
+%
+%   Detailed explanation goes here
+
+opt_inputs={'extra_condition',1,'print_residual_info',0};
+ind_omega = length(branch_point.parameter);
+par_cont_ind = [scan_parm_ind, ind_omega];
+
+[scanned_branch,~]=SetupStst(funcs,'contpar',par_cont_ind,'corpar',ind_omega,...
+    'x',branch_point.x,'parameter',branch_point.parameter,opt_inputs{:},...
+    step_bound_options{:} );
+
+scanned_branch.method.continuation.plot=0;
+[scanned_branch,~,~,~]=br_contn(funcs,scanned_branch,scan_point_count);
+
+scanned_branch.method.stability.minimal_real_part = min_real_part;
+[nunst_scanned_branch,~,~,scanned_branch.point]=GetStability(scanned_branch,...
+'exclude_trivial',true,'locate_trivial',@(p)0,'funcs',funcs);
+
+
+end
+
