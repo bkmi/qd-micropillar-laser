5a8655944e80b21b0911b32106bac32ff177b554
Ben M <benmiller1315@gmail.com>
2016-11-30 16:49:34 +0100

    Add param scanner, inprogress paramSpace plot
    alternate_param_scanner is designed to create a slice along a new parameter at a point on an already created branch
    
    plot_paramSpace is a plotting program which can make a plot based on user defined parameter changes. Relevant to the scanner so you can see first and second continuation params on a single plot.

--- functions/alternate_param_scanner.m
+++ functions/alternate_param_scanner.m
@@ -1,6 +1,9 @@
 function [ scanned_branch, nunst_scanned_branch ] = alternate_param_scanner( branch_point, funcs, scan_point_count, scan_parm_ind, step_bound_options, min_real_part )
 %
-%   Detailed explanation goes here
+%   Input: ( branch_point, funcs, scan_point_count, scan_parm_ind, step_bound_options, min_real_part )
+%   
+%   step_bound_options = { 'step',1,'max_step',[ind_,1],'newton_max_iterations',10, ...
+%                          'min_bound',[ind_,1], 'max_bound',[ind_,2] }
 
 opt_inputs={'extra_condition',1,'print_residual_info',0};
 ind_omega = length(branch_point.parameter);
@@ -12,11 +15,12 @@ par_cont_ind = [scan_parm_ind, ind_omega];
 
 scanned_branch.method.continuation.plot=0;
 [scanned_branch,~,~,~]=br_contn(funcs,scanned_branch,scan_point_count);
+scanned_branch=br_rvers(scanned_branch);
+scanned_branch=br_contn(funcs,scanned_branch,scan_point_count);
 
 scanned_branch.method.stability.minimal_real_part = min_real_part;
 [nunst_scanned_branch,~,~,scanned_branch.point]=GetStability(scanned_branch,...
 'exclude_trivial',true,'locate_trivial',@(p)0,'funcs',funcs);
-
 
 end
 
