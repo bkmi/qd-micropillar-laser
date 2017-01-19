cf0b0b24ae0f07a2c9a7f336ff04c7dc72221548
Ben M <benmiller1315@gmail.com>
2017-01-19 17:08:05 +0100

    Fixed cond, rhs, and init
    modified init_branch, up to a point, to work with omega1 & omega2
    
    added rot_cond, rot_rhs support for multiple omegas and multiple rotations.
    	NOTE: I HAD to forge multiple sys_conds for each rotation.
    	biftool will only do corrections on new parameters if there is a sys_cond related to that param!!
    
    Not a GREAT place to stop and push, but I want to so I don't lose my results.
    My future pushes will be slightly better.

--- rot/self_rot_cond.m
+++ rot/self_rot_cond.m
@@ -7,18 +7,63 @@ function [ res, J ] = self_rot_cond( point, A_rot )
 %       res, ...
 %       J
 
-if isfield(point,'x')
-    % Generally
-    res=0; % residual is always zero
-    J=p_axpy(0,point,[]); % create a 'zero' point, used as the deriv
-    J.x=A_rot*point.x; % Update zero point with deriv = Arot*orig_point
-elseif strcmp(point.kind, 'psol')
-    % For psol, there is more in rot_cond
-    error('Not yet supported!')
-else
-    error('?!?!')
+if isa(A_rot,'double')
+    % Given only one rotation matrix!
+    if isfield(point,'x')
+        % Generally
+        res=0; % residual is always zero
+        J=p_axpy(0,point,[]); % create a 'zero' point, used as the deriv
+        J.x=A_rot*point.x; % Update zero point with deriv = Arot*orig_point
+    elseif strcmp(point.kind, 'psol')
+        % For psol, there is more in rot_cond
+        error('Not yet supported!')
+    else
+        error('?!?!')
+    end
+elseif isa(A_rot,'cell')
+% THIS ISN'T CORRECT. YOU HAVE TO GET THE ROTATION FROM EACH MATRIX + THE J
+% FROM EACH MATRIX
+%     % Given more rotation matricies!
+%     TOTAL_rot = zeros(size(A_rot{1}));
+%     for i = 1:numel(A_rot) %number of elements of okay
+%         TOTAL_rot = TOTAL_rot + A_rot{i};
+%     end
+    
+    
+    % Later on, somewhere in p_correc/correct_ini DDE BIF-TOOL NEEDS
+    % another jacobian column(or row??) to continue each new parameter.
+    % I.e. one for
+    % omega1, one for omega2, etc...
+    % 
+    % When the rot_cond func calls the orig_sys_cond func then it generates
+    % another jacobian column(row?) for each one. For reference I put the
+    % code below:
+    % [userres,userJ]=usercond(userpoint);
+    % J=[userJ(:);Jphas]; (Jphas == This J from rot_cond)
+    
+    
+    if isfield(point,'x')
+        % Generally
+        res_one=0; % residual is always zero
+        res_two=0;
+        % First matrix
+        J_one=p_axpy(0,point,[]); % create a 'zero' point, used as the deriv
+        J_one.x=A_rot{1}*point.x; % Update zero point with deriv = Arot*orig_point
+        % Second Matrix
+        J_two=p_axpy(0,point,[]); % create a 'zero' point, used as the deriv
+        J_two.x=A_rot{2}*point.x; % Update zero point with deriv = Arot*orig_point
+        
+    elseif strcmp(point.kind, 'psol')
+        % For psol, there is more in rot_cond
+        error('Not yet supported!')
+    else
+        error('?!?!')
+    end
+    
+    res = [res_one;res_two];
+    J = [J_one; J_two];
+    
 end
-
 
 end
 
--- rot/self_rot_rhs.m
+++ rot/self_rot_rhs.m
@@ -15,43 +15,92 @@ function [ y ] = self_rot_rhs(xx,p,A,expA,user_rhs,user_tau,isvec)
 %
 % (c) DDE-BIFTOOL v. 3.1.1(20), 11/04/2014
 %
-omega=p(end);
-userpar=p(1:end-1);
-xxrot=xx;
-tau_ind=user_tau();
-if ~isvec
-    for i=2:size(xx,2)
-        xxrot(:,i)=expA(-omega*p(tau_ind(i-1)))*xxrot(:,i);
+
+if isa(A,'double')
+    % USED TO WORK THIS WAY, NEW VERSION BELOW IS FOR MULTIVARIABLE EXPA
+    omega=p(end);
+    userpar=p(1:end-1);
+    xxrot=xx;
+    tau_ind=user_tau();
+    if ~isvec
+        for i=2:size(xx,2)
+            xxrot(:,i)=expA(-omega*p(tau_ind(i-1)))*xxrot(:,i);
+        end
+    else
+        dim=size(xxrot,1); % Number of rows
+        nvec=size(xxrot,3); % Number of 'sheets' -- vectorized
+        for i=2:size(xx,2) % 2:(number of columns)
+    %         xxrot(:,i,:) % all rows, second column, all pages 
+    %         expA(-omega*p(tau_ind(i-1))) %expA @ tau_fb1
+    %         reshape(xxrot(:,i,:),dim,nvec) % Turn multiple sheets into a 2d matrix
+            xxrot(:,i,:)=expA(-omega*p(tau_ind(i-1)))*reshape(xxrot(:,i,:),dim,nvec);
+            % multiply the matricies and move the resulting columns into the
+            % relevant page.
+            % 
+            % The whole process extracted the second column from each page, did
+            % the multiplication, then put each multiplied second column back
+            % where it came from.
+        end
     end
-else
-    dim=size(xxrot,1); % Number of rows
-    nvec=size(xxrot,3); % Number of 'sheets' -- vectorized
-    for i=2:size(xx,2) % 2:(number of columns)
-%         xxrot(:,i,:) % all rows, second column, all pages 
-%         expA(-omega*p(tau_ind(i-1))) %expA @ tau_fb1
-%         reshape(xxrot(:,i,:),dim,nvec) % Turn multiple sheets into a 2d matrix
-        xxrot(:,i,:)=expA(-omega*p(tau_ind(i-1)))*reshape(xxrot(:,i,:),dim,nvec);
-        % multiply the matricies and move the resulting columns into the
-        % relevant page.
-        % 
-        % The whole process extracted the second column from each page, did
-        % the multiplication, then put each multiplied second column back
-        % where it came from.
+    y0=user_rhs(xxrot,userpar); % calculate: given new, rotated 2nd column
+    % y0 == f(y(t),exp(-A*omega*tau1)*y(t-tau1),...)
+
+
+    % A*omega*reshape(xxrot(:,1,:),[dim,nvec])
+    % This thing is the -A*w*y(t) part
+
+    % reshape(A*omega*reshape(xxrot(:,1,:),[dim,nvec]),[dim,1,nvec])
+    % This thing puts it back into pages
+
+    y=y0-reshape(A*omega*reshape(xxrot(:,1,:),[dim,nvec]),[dim,1,nvec]);
+elseif isa(A,'cell')
+    % GIVEN TWO ROTATIONS AND EXPA WITH TWO VAR
+    
+    TOTAL_rot = zeros(size(A{1}));
+    for i = 1:numel(A) %number of elements of okay
+        TOTAL_rot = TOTAL_rot + A{i};
     end
+    
+    omega1=p(end);
+    omega2=p(end-1);
+    userpar=p(1:end-2);
+    xxrot=xx;
+    tau_ind=user_tau();
+    if ~isvec
+        for i=2:size(xx,2)
+            xxrot(:,i)=expA(-omega1*p(tau_ind(i-1)),-omega2*p(tau_ind(i-1)))*xxrot(:,i);
+        end
+    else
+        dim=size(xxrot,1); % Number of rows
+        nvec=size(xxrot,3); % Number of 'sheets' -- vectorized
+        for i=2:size(xx,2) % 2:(number of columns)
+    %         xxrot(:,i,:) % all rows, second column, all pages 
+    %         expA(-omega*p(tau_ind(i-1))) %expA @ tau_fb1
+    %         reshape(xxrot(:,i,:),dim,nvec) % Turn multiple sheets into a 2d matrix
+            xxrot(:,i,:)=expA(-omega1*p(tau_ind(i-1)),-omega2*p(tau_ind(i-1)))*reshape(xxrot(:,i,:),dim,nvec);
+            % multiply the matricies and move the resulting columns into the
+            % relevant page.
+            % 
+            % The whole process extracted the second column from each page, did
+            % the multiplication, then put each multiplied second column back
+            % where it came from.
+        end
+    end
+    y0=user_rhs(xxrot,userpar); % calculate: given new, rotated 2nd column
+    % y0 == f(y(t),exp(-A*omega*tau1)*y(t-tau1),...)
+
+
+    % A*omega*reshape(xxrot(:,1,:),[dim,nvec])
+    % This thing is the -A*w*y(t) part
+
+    % reshape(A*omega*reshape(xxrot(:,1,:),[dim,nvec]),[dim,1,nvec])
+    % This thing puts it back into pages
+
+    y=y0-reshape((A{1}*omega1+A{2}*omega2)*reshape(xxrot(:,1,:),[dim,nvec]),[dim,1,nvec]);
 end
-y0=user_rhs(xxrot,userpar); % calculate: given new, rotated 2nd column
-% y0 == f(y(t),exp(-A*omega*tau1)*y(t-tau1),...)
 
 
-% A*omega*reshape(xxrot(:,1,:),[dim,nvec])
-% This thing is the -A*w*y(t) part
 
-% reshape(A*omega*reshape(xxrot(:,1,:),[dim,nvec]),[dim,1,nvec])
-% This thing puts it back into pages
-
-y=y0-reshape(A*omega*reshape(xxrot(:,1,:),[dim,nvec]),[dim,1,nvec]);
-
-%assignin('base','sample',{xx,p,A,expA,user_rhs,user_tau,isvec})
 
 end
 
