c354852a758a8f91f6a772b8ec3ca7d17615520c
Ben <benmiller1315@gmail.com>
2017-03-12 11:00:23 +0100

    Added biftool, updated path

--- addpath_setup.m
+++ addpath_setup.m
@@ -5,10 +5,10 @@ present_working_directory = pwd;
 addpath(strcat(present_working_directory,'/functions/'))
 addpath(strcat(present_working_directory,'/functions/numerical_systems/'))
 addpath(strcat(present_working_directory,'/scripts/'))
-addpath('~/dde_biftool_v3.1.1/ddebiftool/'); 
-addpath('~/dde_biftool_v3.1.1/ddebiftool_extra_psol/');
-addpath('~/dde_biftool_v3.1.1/ddebiftool_utilities/');
-addpath('~/dde_biftool_v3.1.1/ddebiftool_extra_rotsym');
+addpath(strcat(present_working_directory, '/dde_biftool_v3.1.1/ddebiftool/')); 
+addpath(strcat(present_working_directory, '/dde_biftool_v3.1.1/ddebiftool_extra_psol/'));
+addpath(strcat(present_working_directory, '/dde_biftool_v3.1.1/ddebiftool_utilities/'));
+addpath(strcat(present_working_directory, '/dde_biftool_v3.1.1/ddebiftool_extra_rotsym'));
 
 end
 
